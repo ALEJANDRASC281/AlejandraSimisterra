3175b939d49f7211b66223eb3bed2fc5d05e7920
ALEJANDRASC281 <alejandra.simisterra@utelvt.edu.ec>
2022-11-17 13:59:49 -0500

    modifiqué el link con los integrantes de mi grupo

--- index.php
+++ index.php
@@ -16,16 +16,16 @@ include("marco-right.php");
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Stalin Francis<br>
-                Fecha: 2021-08-17 <br>
+                Posteado por:  Alejandra Simisterra<br>
+                Fecha: 2022-11-17 <br>
           </div>
         </div>
     </div>
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy<b>Alejandra Simisterra</b>,estudiante de la Facultad de Ingenierías en la Universidad Técnica de Esmeraldas “Luis Vargas Torres” de la carrera de Tecnologías de la Información. Estoy en séptimo semestre. Mis padres son Martha Charcopa y Alejandro Simisterra, tengo 9 hermanas y de entre todas soy la menor. Sus nombres son Leisy, Geovanna, Patricia, Estela, Carmen, María, Elisa, y Mónica. </p>
-                   <p class="eys-parrafo">Me gradúe como Técnica en Administracion de Sistemas, tuve profesores que me hicieron amar la programación. Tenían mucha paciencia y nos ayudaban a ser creativos, creamos paginas web, aplicaciones de escritorio, juegos. Todas esas eran cosas que no sabía que alguna vez yo iba a poder hacer solo con el uso de unas cuantas plataformas y una computadora. Los profesores me mostraron ese nuevo mundo y desde entonces lo amé. </p>
+                   <p class="eys-parrafo">Hola, yo soy<b> Alejandra Simisterra</b>,estudiante de la Facultad de Ingenierías en la Universidad Técnica de Esmeraldas “Luis Vargas Torres” de la carrera de Tecnologías de la Información. Estoy en séptimo semestre. Mis padres son Martha Charcopa y Alejandro Simisterra, tengo 9 hermanas y de entre todas soy la menor. Sus nombres son Leisy, Geovanna, Patricia, Estela, Carmen, María, Elisa, y Mónica. </p>
+                   <p class="eys-parrafo">Me gradúe como Técnica en Administración de Sistemas, tuve profesores que me hicieron amar la programación. Tenían mucha paciencia y nos ayudaban a ser creativos, creamos páginas web, aplicaciones de escritorio, juegos. Todas esas eran cosas que no sabía que alguna vez yo iba a poder hacer solo con el uso de unas cuantas plataformas y una computadora. Los profesores me mostraron ese nuevo mundo y desde entonces lo amé. </p>
 <p class="eys-parrafo">Elegí esta carrera porque me gustaba la programación y las distintas cosas que se crean con la ayuda de la tecnología. El mundo actual depende la tecnología y me parece increíble poder formar parte de algo o construir algo en el futuro, ya sea una aplicación u otra cosa.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
@@ -34,23 +34,23 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en la Unidad Educativa Salesiana Maria Auxiliadora, me gradue en el año 2018 como Tecnica en Administracion en Sistemas. Entre sin saber de que trataba exactamente esta especialidad y fue definitivamente un mundo nuevo para mi. El conocer lo que hay detras de la aplicacion o pagina web que reciben los usuarios es increible. Los profesores lograron que me interesara esta especialidad y lograron que aprenda algunos lenguajes de programacion en su forma basica.</p>
-                   <p class="eys-parrafo">En el colegio logre crear varias aplicaciones desde tiendas de ropa  inventarios, y una aplicacion que fue donada a la iglesia de la comunidad. Esta aplicacion ayudaba a llevar los registros que ingresaban a la igleasia y que antes eran puestos o escritos en papel. Tambien logre hacer varias paginas web de distintos temas y fue esto lo que mas me gusto</p>
-<p class="eys-parrafo"> Por el amor que pude agarrarle a la programacion elegi estudiar Tecnologias de la ingformacion en la UTELVT, todo lo que se de programacion es lo que logre aprender en el colegio. En la universidad he reforzado poco nuevos conocimientos.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios los realicé en la Unidad Educativa Salesiana María Auxiliadora, me gradué en el año 2018 como Técnica en Administración en Sistemas. Entré sin saber de qué trataba exactamente esta especialidad y fue definitivamente un mundo nuevo para mi. El conocer lo que hay detras de la aplicación o página web que reciben los usuarios es increible. Los profesores lograron que me interesara esta especialidad y lograron que aprenda algunos lenguajes de programacion en su forma basica.</p>
+                   <p class="eys-parrafo">En el colegio logré crear varias aplicaciones desde tiendas de ropa  inventarios, y una aplicacion que fue donada a la iglesia de la comunidad. Esta aplicación ayudaba a llevar los registros que ingresaban a la iglesia y que antes eran puestos o escritos en papel. Tambien logré hacer varias páginas web de distintos temas y fue esto lo que mas me gustó</p>
+<p class="eys-parrafo"> Por el amor que pude agarrarle a la programación elegí estudiar Tecnologías de la ingformación en la UTELVT, todo lo que se de programación es lo que logré aprender en el colegio. En la universidad he reforzado poco nuevos conocimientos.</p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil personal</h2> 
     
 
 
-                   <p  class="eys-parrafo">Soy Alejandra Simisterra, naci el 22 de agosto del año 2000. Actualmente tengo 22 años de edad y soy una estudiante universitaria. Aunque actualmente no estoy trabajando anteriormente lo estuve haciendo de oficinista donde aprendi muchas cosas, tambien trabaje como mesera en un complejo turistico..</p> 
-                   <p  class="eys-parrafo">Soy una persona muy centrada, me gusta ser responsable en todos los ambitos de mi vida personal y estudiantil. Soy muy exigente porque tambien soy perfeccionista. Me gusta que las cosas queden de la mejor manera posible cuando mi nombre y mi persona estan involucrado en algo.</p>
+                   <p  class="eys-parrafo">Soy Alejandra Simisterra, nací el 22 de agosto del año 2000. Actualmente tengo 22 años de edad y soy una estudiante universitaria. Aúnque actualmente no estoy trabajando anteriormente lo estuve haciendo de oficinista donde aprendí muchas cosas, también trabajé como mesera en un complejo turístico..</p> 
+                   <p  class="eys-parrafo">Soy una persona muy centrada, me gusta ser responsable en todos los ámbitos de mi vida personal y estudiantil. Soy muy exigente porque también soy perfeccionista. Me gusta que las cosas queden de la mejor manera posible cuando mi nombre y mi persona estan involucrado en algo.</p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-		   <p   class="eys-parrafo">Me gusta leer, amo comprar libros y verlos en mi colección. También amo escuchar música en cualquier momento, la música puede ser distinta dependiendo el estado de animo que tenga en el instante. Amo escuchar discos de cualquier genero musical. Otra cosa que me gusta es coleccionar photocards o tarjetas, aunque es uno de mis pasatiempos mas recientes creo que es uno de los que más me gusta. Por último, me gusta estar informada de todo lo que pueda en cuanto a avances se refiere. Me gusta poder conocer muchas cosas..</p>
+		   <p   class="eys-parrafo">Me gusta leer, amo comprar libros y verlos en mi colección. También amo escuchar música en cualquier momento, la música puede ser distinta dependiendo el estado de animo que tenga en el instante. Amo escuchar discos de cualquier genero musical. Otra cosa que me gusta es coleccionar photocards o tarjetas, aunque es uno de mis pasatiempos más recientes creo que es uno de los que más me gusta. Por último, me gusta estar informada de todo lo que pueda en cuanto a avances se refiere. Me gusta poder conocer muchas cosas..</p>
        
 
                    
@@ -80,13 +80,13 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Stalin"; 
+ document.title="Alejandra"; 
 document.getElementById("foto").src="./images/stalin.png";
-document.getElementById("elnombre").innerHTML="Stalin Francis";
+document.getElementById("elnombre").innerHTML="Alejandra Simisterra";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='https://highkickesmeraldas.com/BalcazarLuis/'>Luis Balcazar</a></p><p><a href='https://highkickesmeraldas.com/SusanaCharcopa/'>Susana Charcopa</a></p><p><a href='  '>Juan Zambrano </a> </p><p><a href>Gabriel Quinde</a> </p>   " ;
  </script>
  
 <?php
